1431959935eb9705f06aaa04c8f2f39fe6908cee
Juan Villamil <jd.villamil@uniandes.edu.co>
2019-09-14 16:42:38 -0500

    Correcciones listaEncadenada

--- src/model/data_structures/ListaSencillamenteEncadenada.java
+++ src/model/data_structures/ListaSencillamenteEncadenada.java
@@ -1,9 +1,8 @@
 package model.data_structures;
 
-import java.util.Collection;
+
 import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+
 
 public class ListaSencillamenteEncadenada<E> extends ListaEncadenadaAbstracta<E>
 {
@@ -203,10 +202,27 @@ public class ListaSencillamenteEncadenada<E> extends ListaEncadenadaAbstracta<E>
 	}
 
 	@Override
-	public int indexOf(E element) 
+	public int indexOf(E o) 
 	{
-		//TODO Pendiente
-		return 0;
+		int posicion = -1;
+		Nodo<E> actual = primerNodo;
+		int posActual = 0;
+		boolean listo = false;
+		while(actual != null && !listo)
+		{
+			if(actual.darElemento().equals((o)))
+			{
+				posicion = posActual;
+				listo = true;
+			}
+			else
+			{
+				posActual ++;
+				actual = actual.darSiguiente();
+			}
+		}
+
+		return posicion;
 	}
 
 	/**
